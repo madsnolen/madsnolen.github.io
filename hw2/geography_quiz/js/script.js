8f33b45a6a91c9855c11a51193c153437dcf96f4
madisonnolen <madisonnolen03@gmail.com>
2026-03-17 11:00:37 -0700

    10 questions fixed

--- hw2/geography_quiz/js/script.js
+++ hw2/geography_quiz/js/script.js
@@ -6,6 +6,7 @@ var score = 0;
 var attempts = localStorage.getItem("totalAttempts");
 
 displayQ4Choices();
+displayQ8Choices();
 
 //dunction to display  question 4 choices
 function displayQ4Choices(){
@@ -15,6 +16,15 @@ function displayQ4Choices(){
         document.querySelector("#q4Choices").innerHTML += ` <input type="radio" name="q4" id= "${q4ChoicesArray[i]}" value="${q4ChoicesArray[i]}"> <label for="${q4ChoicesArray[i]}"> ${q4ChoicesArray[i]}</label>`;    
 }
 }//displayQ4Choices
+
+//dunction to display  question 8 choices
+function displayQ8Choices(){
+    let q8ChoicesArray = ["Florida", "Hawaii", "California", "New York"];
+    q8ChoicesArray = _.shuffle(q8ChoicesArray);
+    for(let i=0; i < q8ChoicesArray.length; i++){
+        document.querySelector("#q8Choices").innerHTML += ` <input type="radio" name="q8" id= "${q8ChoicesArray[i]}" value="${q8ChoicesArray[i]}"> <label for="${q8ChoicesArray[i]}"> ${q8ChoicesArray[i]}</label>`;    
+}
+}//displayQ8Choices
 
 
 
@@ -42,10 +52,22 @@ function gradeQuiz(){
  let q2Reponse = document.querySelector("#q2").value;
  let q4Reponse = document.querySelector("input[name=q4]:checked").value;
  let q5Reponse = document.querySelector("#q5").value.toLowerCase();
+ let q6Reponse = document.querySelector("#q6").value;
+ let q8Reponse = document.querySelector("input[name=q8]:checked").value;
+ let q9Reponse = document.querySelector("#q9").value.toLowerCase();
+ let q10Reponse = document.querySelector("#q10").value.toLowerCase();
+
+
 
  console.log(q1Reponse);
  console.log(q2Reponse);
  console.log(q4Reponse);
+ console.log(q5Reponse);
+ console.log(q6Reponse);
+ console.log(q7Reponse);
+ console.log(q8Reponse);
+ console.log(q9Reponse);
+ console.log(q10Reponse);
 
  //grading question 1
  if(q1Reponse == "sacramento"){
@@ -86,6 +108,45 @@ else{
     wrongAnswer(5);
 }
 
+//grade question 6
+ if(q2Reponse == "al"){
+   rightAnswer(6);
+}
+else{
+    wrongAnswer(6);
+}
+
+//grade question 7
+if(document.querySelector("#Florida").checked && document.querySelector("#South Carolina").checked && !document.querySelector("#Texas").checked && document.querySelector("#New Hampshire").checked){
+    rightAnswer(7);
+}
+else{
+    wrongAnswer(7);
+}
+
+//grade question 8
+if(q8Reponse == "Florida"){
+    rightAnswer(8);
+}else{
+    wrongAnswer(8);
+}
+
+//grade question 9
+ if(q1Reponse == "california"){
+    rightAnswer(9);
+}
+else{
+    wrongAnswer(9);
+}
+
+//grade question 10
+ if(q10Reponse == "6"){
+    rightAnswer(10);
+}
+else{
+    wrongAnswer(10);
+}
+
 
 document.querySelector("#totalScore").innerHTML = `Total Score: ${score}`;
 //add if statement here to Display the Total Score message in red if the score is lower than 80 or in green otherwise.
@@ -110,7 +171,7 @@ function rightAnswer(index){
     document.querySelector(`#q${index}Feedback`).innerHTML = "Correct!";
     document.querySelector(`#q${index}Feedback`).className = "bg-success text-white";
     document.querySelector(`#markImg${index}`).innerHTML ="<img src='img/checkmark.png'>";
-    score += 20;
+    score += 10;
 }
 
 function wrongAnswer(index){
